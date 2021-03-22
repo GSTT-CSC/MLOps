9b4621b1711fe0d8d51abe741f015e6465acb3c9
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-03-22 17:53:44 +0000

    fixes run_project errors messages when project exists
    Switched order for get/create experiment so error messages aren't generated when project exists

--- run_project.py
+++ run_project.py
@@ -5,10 +5,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
-# Get these from config file eventually
-experiment_name='mlflow_tests_12'
-artifact_path='s3://mlflow'
-remote_server_uri='http://localhost:80'
+experiment_name = 'mlflow_tests_16'
+artifact_path = 's3://mlflow'
+remote_server_uri = 'http://localhost:80'
 
 os.environ['MLFLOW_TRACKING_URI'] = remote_server_uri
 os.environ['MLFLOW_S3_ENDPOINT_URL'] = config['server']['MLFLOW_S3_ENDPOINT_URL']
@@ -18,13 +17,21 @@ os.environ['AWS_SECRET_ACCESS_KEY'] = config['user']['AWS_SECRET_ACCESS_KEY']
 
 def init_experiment(name, artifact_location=artifact_path):
     # Create an experiment name, which must be unique and case sensitive
-    try:
-        experiment_id = mlflow.create_experiment(name, artifact_location=artifact_location)
-        print('Created experiment: {}'.format(name))
-    except mlflow.exceptions.MlflowException:
-        print("experiment already exists: logging to existing")
 
-    experiment_id = mlflow.set_experiment(name)
+    experiment = mlflow.get_experiment_by_name(name)
+
+    if experiment is None:
+        experiment_id = mlflow.create_experiment(name, artifact_location=artifact_location)
+        experiment = mlflow.get_experiment(experiment_id)
+        print('Creating experiment: name: {0} *** ID: {1}'.format(experiment.name, experiment.experiment_id))
+    else:
+        experiment_id = experiment.experiment_id
+        print('Logging to experiment: {0} *** ID: {1}'.format(experiment.name, experiment.experiment_id))
+
+    return experiment_id
+
+
+def print_experiment_info(experiment_id):
     experiment = mlflow.get_experiment(experiment_id)
     print("Name: {}".format(experiment.name))
     print("Experiment_id: {}".format(experiment.experiment_id))
@@ -33,6 +40,7 @@ def init_experiment(name, artifact_location=artifact_path):
     print("Lifecycle_stage: {}".format(experiment.lifecycle_stage))
 
 
-init_experiment(experiment_name)
-# Run MLflow test_project and create a reproducible conda environment
-mlflow.run('.', docker_args={'network': 'host', 'rm': ''}, use_conda=False)
+# Configure experiment and run project
+experiment_id = init_experiment(experiment_name)
+print_experiment_info(experiment_id)
+mlflow.run('.', docker_args={'network': 'host', 'rm': ''}, use_conda=False, experiment_id=experiment_id)
