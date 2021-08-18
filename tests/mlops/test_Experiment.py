ce4600cb26ddcea82376db5e4d22d8b7d1de2f62
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-08-18 15:00:13 +0100

    Add framework for tests
    Added tests functions, currently all skipped while testing Github actions automated testing.

---
+++ tests/mlops/test_Experiment.py
@@ -0,0 +1,40 @@
+from mlops.Experiment import Experiment
+import pytest
+
+
+class TestExperiment:
+
+    def setup(self):
+        self.experiment = Experiment('tests/config/config.cfg')
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_config_setup(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_read_config(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_init_experiment(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_print_experiment_info(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_configure_minio(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_build_experiment_image(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_build_project_file(self):
+        pass
+
+    @pytest.mark.skip(reason="placeholder for test")
+    def test_run(self):
+        pass
