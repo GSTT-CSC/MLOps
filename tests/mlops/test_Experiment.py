a9d3bc0f68df6d3fd546ba7a114a4315ce22b7bd
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-10-28 14:19:31 +0100

    add test for env check

--- tests/mlops/test_Experiment.py
+++ tests/mlops/test_Experiment.py
@@ -3,6 +3,7 @@ import configparser
 from minio import Minio
 import os
 import docker
+import pytest
 
 
 class TestExperiment:
@@ -13,6 +14,14 @@ class TestExperiment:
         os.environ['MINIO_ROOT_USER'] = 'minioadmin'
         os.environ['MINIO_ROOT_PASSWORD'] = 'minioadmin'
         self.experiment = Experiment('tests/data/test_config.cfg', use_localhost=use_localhost)
+
+    def test_check_environment_variables(self):
+        test_var = os.environ['MINIO_ROOT_PASSWORD']
+        os.environ['MINIO_ROOT_PASSWORD'] = ''
+        with pytest.raises(Exception) as e:
+            self.experiment.check_environment_variables()
+        # reset env var
+        os.environ['MINIO_ROOT_PASSWORD'] = test_var
 
     def test_config_setup(self):
         self.experiment.config_setup()
