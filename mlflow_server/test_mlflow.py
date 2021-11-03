b7a607985ba75144401033783df9ea0bb1138b1e
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-11-03 19:02:26 +0000

    Update test_mlflow.py

--- mlflow_server/test_mlflow.py
+++ mlflow_server/test_mlflow.py
@@ -1,7 +1,20 @@
 import mlflow
+import os
 
+experiment_name = 'test experiment'
 mlflow.set_tracking_uri("http://localhost:85")
-mlflow.create_experiment("test_experiment", artifact_location='s3://mlflow/test')
+
+experiment = mlflow.get_experiment_by_name(experiment_name)
+if experiment is None:
+    exp_id = mlflow.create_experiment(experiment_name, artifact_location=artifact_path)
+    print('Creating experiment: name: {0} *** ID: {1}'.format(experiment_name, exp_id))
+else:
+    exp_id = experiment.experiment_id
+    print('Logging to existing experiment: {0} *** ID: {1}'.format(experiment_name, exp_id))
+
+mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+mlflow.set_experiment(experiment_name)
+
 
 with mlflow.start_run() as run:
     mlflow.log_metric("test", 0)
