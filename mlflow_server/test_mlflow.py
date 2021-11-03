abe6873f283f41e650a21715dd6354fde2e7c319
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-11-03 18:52:51 +0000

    update test file

--- mlflow_server/test_mlflow.py
+++ mlflow_server/test_mlflow.py
@@ -1,13 +1,15 @@
 import mlflow
 
 mlflow.set_tracking_uri("http://localhost:85")
-mlflow.set_experiment("test_experiment")
+mlflow.create_experiment("test_experiment", artifact_location='s3://mlflow/test')
 
 with mlflow.start_run() as run:
     mlflow.log_metric("test", 0)
 
 
 with mlflow.start_run() as run:
+    artifact_uri = mlflow.get_artifact_uri()
+    print("Artifact uri: {}".format(artifact_uri))
     mlflow.log_artifact("/home/hnadmin/MLOps/mlflow_server/test_mlflow.py")
 
 mlflow.end_run()
