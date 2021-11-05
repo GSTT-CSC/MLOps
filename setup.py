89c5ee15d1b3d4e61af29d95ed4958468e221d64
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2021-11-05 12:02:12 +0000

    Include torchserve requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='mlops',
-   version='0.2',
+   version='0.3.2',
    description='Server and base class for mlops experiments',
    author='Laurence Jackson',
    packages=find_packages(),
