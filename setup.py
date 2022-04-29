4616081967413d778fda1eed26f6368203530379
Laurence Jackson <33687657+laurencejackson@users.noreply.github.com>
2022-04-29 12:06:44 +0100

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='mlops',
-   version='0.3.3',
+   version='0.4.1',
    description='Server and base class for mlops experiments',
    author='Laurence Jackson',
    packages=find_packages(),
