c3148cb3d292a1f4ea2a0aa4f68f7ef79ad17d16
Laurence Jackson <33687657+laurencejackson@users.noreply.github.com>
2022-05-30 13:46:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.5.6"
+version = "0.5.7"
 
 setuptools.setup(
     name="csc-mlops",
