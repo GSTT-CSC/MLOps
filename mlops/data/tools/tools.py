f04e7338b7cef3fd625b1c47b144683218f6290b
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2022-10-14 16:02:04 +0100

    Update tools.py

--- mlops/data/tools/tools.py
+++ mlops/data/tools/tools.py
@@ -24,21 +24,25 @@ def xnat_build_dataset(xnat_configuration: dict, actions: list = None, structure
             data_series = []
             if actions:
                 for action, data_label in actions:
-                    #  No actions, just return a list of subject IDs and URIs
-                    logger.debug(f"Running action: {action.__name__} on {subject}")
-                    xnat_obj = action(project.subjects[subject])
-                    output = []
-                    for obj in xnat_obj:
-                        output.append({'source_action': action.__name__,
-                                       'xnat_uri': obj.uri,
-                                       'data_label': data_label,
-                                       'subject_uri': project.subjects[subject].uri,
-                                       'subject_id': project.subjects[subject].label})
-                    if output:
-                        dataset.append(output)
-                    else:
+                    try:
+                        #  No actions, just return a list of subject IDs and URIs
+                        logger.debug(f"Running action: {action.__name__} on {subject}")
+                        xnat_obj = action(project.subjects[subject])
+                        output = []
+                        for obj in xnat_obj:
+                            output.append({'source_action': action.__name__,
+                                           'xnat_uri': obj.uri,
+                                           'data_label': data_label,
+                                           'subject_uri': project.subjects[subject].uri,
+                                           'subject_id': project.subjects[subject].label})
+                        if output:
+                            dataset.append(output)
+                        else:
+                            logger.warn(f'No suitable data found, ignoring sample')
+                            continue
+                    except TypeError:
                         logger.warn(f'No suitable data found, ignoring sample')
-
+                        continue
             else:
                 #  No actions, just return a list of subject IDs and URIs
                 data_series.append({'subject_uri': project.subjects[subject].uri})
