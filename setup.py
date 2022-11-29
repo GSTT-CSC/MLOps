dfb7e1e50115b7df82eb5dc229cf9b2704559c71
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2022-11-29 17:50:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.9.13"
+version = "0.9.14"
 
 install_requires = [
     'mlflow==2.0.1',
