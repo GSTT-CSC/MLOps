656683ae9d0f61dc73654334c0dcc19dd77a0a01
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2023-05-10 23:10:07 +0100

    register model after training

--- tests/data/test_entry.py
+++ tests/data/test_entry.py
@@ -14,12 +14,8 @@ def test():
         model = torch.nn.Linear(3, 2)
         x = torch.rand(1, 3)
         y = model(x)
-        model = mlflow.pytorch.log_model(model, 'test_model')
+        model = mlflow.pytorch.log_model(model, 'test_model', registered_model_name="test-model",)
         print(f'model logged at {model.model_uri}')
-
-        env_file = os.getenv('GITHUB_ENV')
-        with open(env_file, "a") as f:
-            f.write(f"MLOPS_TEST_MODEL_URI={model.model_uri}")
 
 
 if __name__ == '__main__':
