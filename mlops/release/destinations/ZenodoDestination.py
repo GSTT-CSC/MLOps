35c6fccbaff46a7748dac17768fcd8efb9742690
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2023-05-11 16:49:38 +0100

    outline release workflow

--- mlops/release/destinations/ZenodoDestination.py
+++ mlops/release/destinations/ZenodoDestination.py
@@ -19,7 +19,8 @@ class ZenodoDestination(ReleaseDestination, ABC):
         super(ZenodoDestination, self).__init__()
         self.config = config
         self.params = {"access_token": self.config['access_token']}
-        self.get_url()
+        self.bucket_url = self.get_url()
+        self.remote_artifacts = {}
 
     def get_url(self):
         # Get bucket URL
@@ -29,30 +30,35 @@ class ZenodoDestination(ReleaseDestination, ABC):
             json={},
             headers={"Content-Type": "application/json"}
         )
-        self.bucket_url = res.json()["links"]["bucket"]
+        return res.json()["links"]["bucket"]
 
     def push(self, release_artifacts: dict = None) -> dict:
         """
-        Local destination already has files on disk after collect method called by ReleaseSource, push method has no effect but adds destination paths
+        pushes artifacts to zenodo
         :param release_artifacts:
         :return:
         """
         logger.info(f'Pushing {len(release_artifacts)} items')
-        for art_name, art_path in release_artifacts.items():
-            logger.info(f'Uploading {art_name} ...')
-            file_size = os.stat(art_path).st_size
-            if os.path.isdir(art_path):
-                logger.debug(f'compressing directory {art_name}')
-                art_path = shutil.make_archive(art_path, 'zip', art_path)
+        for artifact_name, artifact_path in release_artifacts.items():
+            logger.info(f'Uploading {artifact_name} ...')
+            file_size = os.stat(artifact_path).st_size
+            if os.path.isdir(artifact_path):
+                logger.debug(f'Compressing directory {artifact_name}')
+                artifact_path = shutil.make_archive(artifact_path, 'zip', artifact_path)
 
-            with open(art_path, "rb") as f:
+            with open(artifact_path, "rb") as f:
                 with tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024) as t:
                     wrapped_file = CallbackIOWrapper(t.update, f, "read")
                     r = requests.put(
-                        self.bucket_url + "/" + os.path.basename(art_path),
+                        self.bucket_url + "/" + os.path.basename(artifact_path),
                         data=wrapped_file,
                         params=self.params,
                     )
 
-        return  r.json()
+            self.remote_artifacts[artifact_name] = r.json()['links']['self']
 
+    def pull(self):
+        """
+        pulls artifacts from destination
+        :return:
+        """
