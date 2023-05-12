7d77a372889dbb9511e4fc3222d9397b9a72799d
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2023-05-12 12:02:46 +0100

    mlops automated release commit

--- tests/mlops/release/data/build_script.sh
+++ tests/mlops/release/data/build_script.sh
@@ -1,1 +1,7 @@
-echo RUNNING BUILD SCRIPT
+echo can use this script to commit to git? or use gitpython? Need UIDs for remote artifacts somewhere!
+export VERSION=$(awk -F "=" '/version/ {print $2}' tests/data/_verison.py | sed 's/ //g')
+echo Building release: $VERSION
+git checkout -b release-demo/${VERSION}
+git add .
+git commit -m "mlops automated release commit"
+git push --set-upstream origin release-demo/${VERSION}
