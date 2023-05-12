ef5ad3bffc0dd5ae2e1932d4cedf574f4a66a3f3
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2023-05-12 12:04:08 +0100

    adds version script

--- tests/mlops/release/data/build_script.sh
+++ tests/mlops/release/data/build_script.sh
@@ -1,5 +1,5 @@
 echo can use this script to commit to git? or use gitpython? Need UIDs for remote artifacts somewhere!
-export VERSION=$(awk -F "=" '/version/ {print $2}' tests/data/_verison.py | sed 's/ //g')
+export VERSION=$(awk -F "=" '/version/ {print $2}' tests/data/_version.py | sed 's/ //g')
 echo Building release: $VERSION
 git checkout -b release-demo/${VERSION}
 git add .
